76ba5a4bacab6c377095be0db6b6224f5f2b2770
Aaris-Kazi <aariskazi@gmail.com>
2026-07-24 18:47:54 +0530

    UPDATE :: config add

--- src/components/utills/Config.ts
+++ src/components/utills/Config.ts
@@ -2,7 +2,8 @@ const config = {
     BASE_URL:"http://192.168.0.102:8088",
     HEADER: { "Content-Type": "application/json" },
     TIMEOUT: 5000,
-    OK_STATUS: 200
+    OK_STATUS: 200,
+    AUTH: "/api/auth"
 };
 
 export default config;
