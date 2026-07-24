427bd88bb7a5fe36f9566f7db021d6f757032e1b
Aaris-Kazi <aariskazi@gmail.com>
2026-07-24 20:23:19 +0530

    UPDATE :: adding loader

---
+++ src/components/clientsideComponents/Loader.tsx
@@ -0,0 +1,10 @@
+const Loader = () => {
+    return (
+        <div className="spinner-border loader" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </div>
+
+    )
+}
+
+export default Loader
