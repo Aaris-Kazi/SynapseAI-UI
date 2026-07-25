1189a0926712d3c3fd46772f351fdf2ced708bba
Aaris-Kazi <aariskazi@gmail.com>
2026-07-25 13:12:07 +0530

    UPDATE :: config changes

--- src/components/utills/Config.ts
+++ src/components/utills/Config.ts
@@ -1,5 +1,6 @@
 const config = {
     BASE_URL:"http://192.168.0.102:8088",
+    // BASE_URL:"http://localhost:8080",
     HEADER: { "Content-Type": "application/json" },
     TIMEOUT: 5000,
     OK_STATUS: 200,
