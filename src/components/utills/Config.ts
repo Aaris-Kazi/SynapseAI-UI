165d88723d8d2043ff38332790c4024fc0a72906
Aaris-Kazi <aariskazi@gmail.com>
2026-08-04 17:40:57 +0530

    UPDATE :: config fix

--- src/components/utills/Config.ts
+++ src/components/utills/Config.ts
@@ -2,7 +2,7 @@ const config = {
     BASE_URL:"http://192.168.0.105:8088",
     // BASE_URL:"http://localhost:8080",
     HEADER: { "Content-Type": "application/json" },
-    TIMEOUT: 30000,
+    TIMEOUT: 60000,
     OK_STATUS: 200,
     AUTH: "/api/auth",
     AFTER_LOGIN_PATH: '/chat',
