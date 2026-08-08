8e76ba7de8bc4cd053a035501af4eb649196b9c2
Aaris-Kazi <aariskazi@gmail.com>
2026-08-08 20:52:34 +0530

    UPDATE: logging out

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,12 +1,20 @@
 import { useNavigate } from "react-router-dom";
 import { removeAccessToken } from "../components/utills/ServiceLayer";
 import config from "../components/utills/Config";
+import { useEffect } from "react";
 
 const Logout= () => {
 
     const navigate = useNavigate();
     removeAccessToken();
-    navigate(config.BEFORE_LOGIN_PATH);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate(config.BEFORE_LOGIN_PATH);
+        }, 500);
+        return () => clearTimeout(timer);
+    }, [navigate]);
+
     return (
         <div>
             <h1>Logging out...</h1>
